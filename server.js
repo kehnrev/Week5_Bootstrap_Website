20e25a80e40d0ed1e8781dd7ae7610d655851f8d
kehn0 <kehnrev23@gmail.com>
2025-03-25 08:38:36 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index5.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
